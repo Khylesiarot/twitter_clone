4185fbaa140d1118798fc42ecf74543867565224
khyle_siarot <johnkhyle.siarot.20@usjr.edu.ph>
2022-11-30 19:12:09 +0800

    Homepage nav bar

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,3 +1,6 @@
+import 'dart:html';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget{
@@ -11,9 +14,23 @@ class HomePage extends StatefulWidget{
 }
 
 class Homepage extends State<HomePage> {
+  String src = "https://cdn-icons-png.flaticon.com/512/733/733579.png";
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: 
+          AppBar(
+            centerTitle: true,           backgroundColor: Colors.white,
+            leading: IconButton(onPressed: ()=> {}, icon: Icon(Icons.account_circle_outlined, color: Colors.black, size: 25,),),
+            title: FaIcon( FontAwesomeIcons.twitter , color: Colors.blue, ),
+            actions: [IconButton(onPressed: () => {}, icon: FaIcon(FontAwesomeIcons.bolt), iconSize: 20, color: Colors.black,)],
+
+          ),
+
+      
+        ),
+
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'createacc.dart';
+import 'homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
        
         primarySwatch: Colors.blue,
       ),
-      home: const Create(),
+      home: const HomePage(),
     );
   }
 }
