e4a9549755af10dc12b3d99471dba94f71a16741
khyle_siarot <johnkhyle.siarot.20@usjr.edu.ph>
2022-11-30 17:38:53 +0800

    log in screen and homepage dat file creation

---
+++ lib/createacc.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Create extends StatefulWidget{
+  const Create({super.key});
+
+  
+  @override
+  Createacc createState() => Createacc();
+
+
+}
+
+class Createacc extends State<Create> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget{
+  const HomePage({super.key});
+
+  
+  @override
+  Homepage createState() => Homepage();
+
+
+}
+
+class Homepage extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
